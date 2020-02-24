78b776a5f84551927eb696d15e2c430b02249272
Lorenzo <lollobaldo2000@gmail.com>
2020-02-24 16:56:49 +0100

    Added alphabeta tests

--- Tester.hs
+++ Tester.hs
@@ -9,9 +9,12 @@
 {-# OPTIONS -Wmissing-export-lists #-}
 {-# OPTIONS -Wpartial-fields #-}
 {-# OPTIONS -funbox-strict-fields #-}
+{-# OPTIONS -fhide-source-paths #-}
+{-# OPTIONS -freverse-errors #-}
+
 
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE BangPatterns #-}
+-- {-# LANGUAGE BangPatterns #-}
 module Tester (main) where
 
 import Data.List
@@ -55,7 +58,8 @@ data Function =
   BFS |
   DFS |
   ASS |
-  ABS deriving (Show)
+  ABS |
+  AB deriving (Show)
 
 inputError :: a
 inputError = error "ERROR: wrong input type"
@@ -90,6 +94,9 @@ instance Test TestEval where
 instance Test TestSearch where
   test = testerSearch
 
+instance Test TestAlphaBeta where
+  test AB = testerAlphaBeta
+  test _ = inputError
 
 
 inclusiveIntercalate :: [a] -> [[a]] -> [a]
@@ -158,7 +165,7 @@ getResult f s expected output = unsafePerformIO result
     analyse False = TestError f s (show expected) (show output)
     handleError :: IO Result -> IO Result
     handleError =
-      (flip catches)
+      flip catches
         [Handler (\(_ :: ErrorCall) -> return $ TestError f s (show expected) "UNDEFINED")]--,
         --  Handler (\(_ :: SomeException) -> return $ TestError f s (show expected) "ERROR")]
     execute :: IO (Maybe Result)
@@ -169,8 +176,7 @@ getResult f s expected output = unsafePerformIO result
       timeout timer . handleError . evaluate . analyse $ (output == expected)
     result = do
       let timeoutError = TestError f s (show expected) "TIMEOUT"
-      res <- execute
-      return $ fromMaybe timeoutError res
+      fromMaybe timeoutError <$> execute
 
 testerNext :: TestNext -> Result
 testerNext (TestNext branch graph out) = analyse . prettify $ next inp1 inp2
@@ -237,6 +243,12 @@ testerSearch f testObj = analyse . prettify $ case f of
     depthLimit = testDepth testObj
     heuristicTable = hrTable . graphObj $ testObj
 
+testerAlphaBeta :: TestAlphaBeta -> Result
+testerAlphaBeta (TestAlphaBeta testDescription inp2 inp1 out) = analyse $ alphabeta inp1 inp2
+  where
+    analyse :: Int -> Result
+    analyse = getResult AB testDescription out
+
 testAll :: Test t => Function -> [t] -> [Result]
 testAll = map . test
 
@@ -254,11 +266,11 @@ funcList :: [Function]
 funcList = [BFS, DFS, ASS]
 runTestsSearches :: [[Result]]
 runTestsSearches = map (`testAll` testsListSearch) funcList
-
+                   ++ [testAll AB testsListAlphaBeta]
 
 main :: IO ()
 main = do
-  putStrLn "AAARAINBOWAAA"
+  -- putStrLn "AAARAINBOWAAA"
   args <- getArgs
   -- let user = if null args then "" else head args
   let meme = "--no-meme" `elem` args
@@ -268,7 +280,7 @@ main = do
   putStrLn . tabularise $ labelsFunc
   putStrLn "Testing searches:"
   let testResultsTable = map (map show) runTestsSearches
-  let labels = zipWith (:) ["Breath-first search","Depth-first search","A* search"] testResultsTable
+  let labels = zipWith (:) ["Breath-first search","Depth-first search","A* search", "Alpha-Beta"] testResultsTable
   putStrLn . tabularise $ labels
   let errors = [descriptivePrintResult e | e@TestError{} <- concat (runTestsFunctions ++ runTestsSearches)]
   let passTest = "All tests passed"
